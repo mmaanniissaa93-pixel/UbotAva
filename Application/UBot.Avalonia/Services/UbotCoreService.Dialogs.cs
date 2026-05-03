ee70b9b46f7e192c923103452e5a9f010ae646f8
iizzmmiirr35 <mmaanniissaa93@gmail.com>
2026-05-03 03:29:35 +0300

    Update UbotCoreService.Dialogs.cs

--- Application/UBot.Avalonia/Services/UbotCoreService.Dialogs.cs
+++ Application/UBot.Avalonia/Services/UbotCoreService.Dialogs.cs
@@ -1,37 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
-using UBot.FileSystem;
-using UBot.NavMeshApi;
-using UBot.NavMeshApi.Dungeon;
-using UBot.NavMeshApi.Edges;
-using UBot.NavMeshApi.Extensions;
-using UBot.NavMeshApi.Terrain;
-using UBot.Core.Client.ReferenceObjects;
-using UBot.GameData.ReferenceObjects;
-using UBot.Core;
-using UBot.Core.Components;
-using UBot.Core.Event;
-using UBot.Core.Extensions;
-using UBot.Core.Network;
-using UBot.Core.Network.Protocol;
-using UBot.Core.Objects;
-using UBot.Core.Objects.Party;
-using UBot.Core.Objects.Spawn;
-using UBot.Core.Objects.Skill;
-using UBot.Core.Plugins;
 using Forms = System.Windows.Forms;
-using CoreRegion = UBot.Core.Objects.Region;
 
 namespace UBot.Avalonia.Services;
 
